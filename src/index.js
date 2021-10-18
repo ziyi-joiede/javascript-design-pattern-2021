d4b090c4b46ed2e0db3cfbf60db4404e70d1654b
YoNgWeI <30373219+YoNgWeI1992@users.noreply.github.com>
2021-10-18 23:31:26 +0800

    搭建环境

---
+++ src/index.js
@@ -0,0 +1,12 @@
+class Person {
+	constructor(name) {
+		this.name = name
+	}
+
+	getName() {
+		return this.name
+	}
+}
+
+let p = new Person('双越老师')
+alert(p.getName())
---
+++ webpack.dev.config.js
@@ -0,0 +1,29 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		path: __dirname,
+		filename: './release/bundle.js'
+	},
+	module: {
+		rules: [{
+			test: /\.js?$/,
+			exclude: /(node_modules)/,
+			loader: 'babel-loader'
+		}]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './index.html'
+		})
+	],
+	devServer: {
+		static: {
+			directory: path.join(__dirname, './release/'), // 根目录
+		},
+		open: true, // 自动打开
+		port: 9000  // 端口
+	}
+}
